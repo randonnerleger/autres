a152816d8aacd4baf58516b46d31b476cb05e88c
Frederic Caffin <fc@xymail.fr>
2019-02-24 13:44:33 +0100

    Ajout maintenance.php à la racine pour centraliser les messages de maintenance pour tous les services extérieurs aux forum (wiki, konami, fotoo). L'asso reste pour le moment indépendante.

---
+++ maintenance.php
@@ -0,0 +1,37 @@
+<?php
+require_once ('configRL.php');
+?>
+<head>
+	<title>Maintenance / Randonner-Leger</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="forum/style/RL_Clair.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo path_to_forum ; ?>/style/RL_Clair.css" />
+</head>
+<body>
+
+<div id="punmaint" class="pun">
+<img src="<?php echo folder_rl ; ?>/tpl/img/logo.png" />
+<div class="top-box"></div>
+<div class="punwrap">
+
+<div id="brdmain">
+<div class="block">
+	<h2>Maintenance</h2>
+	<div class="box">
+		<div class="inbox">
+			<p>
+				Le site Randonner-Leger est actuellement en maintenance.<br />
+				Merci de revenir plus tard.
+			</p>
+		</div>
+	</div>
+</div>
+</div>
+
+</div>
+<div class="end-box"></div>
+</div>
+
+</body>
+</html>
