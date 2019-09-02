e344ec17fbd7b077b5574b5a9d4a9b5713cbe419
Frederic Caffin <fc@xymail.fr>
2019-09-02 13:46:19 +0200

    Versioning CSS

--- tetris.php
+++ tetris.php
@@ -8,7 +8,7 @@
 <!-- BEGIN MODIF META RL -->
 <?php include PUN_ROOT.'include/user/header_favicon.php';?>
 <?php include PUN_ROOT.'include/user/header_img_aleatoire.php';?>
-<link rel="stylesheet" type="text/css" href="<?php echo path_to_forum.'style/'.$conf['pun_style'].'.css?' . current_theme . ''; ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo path_to_forum.'style/'.$conf['pun_style'].'.css?version=' . current_theme . ''; ?>" />
 <!-- BEGIN MODIF META RL -->
 
 <!--
