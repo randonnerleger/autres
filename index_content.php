b8eb906b8ccd6211c0cc16ef4567137930f97a2c
Frederic Caffin <fc@xymail.fr>
2019-09-13 17:37:59 +0200

    Création d'une page index.php

---
+++ index_content.php
@@ -0,0 +1,60 @@
+<p>
+En randonnant, un jour ou l'autre on envisage de porter un sac plus léger.
+</p>
+
+<p>
+Cette réflexion a été à l'origine de <a href="https://www.randonner-leger.org/" title="Randonner-léger.org">www.randonner-leger.org</a> qui est maintenant un site interactif accueillant une communauté de passionnés de randonnée légère.
+</p>
+
+<p>
+Nous avons mis en commun des outils et des connaissances pour la promouvoir et la développer, joins-toi à nous !
+</p>
+
+<div class="flex-container">
+
+	<div class="wrap_box plugin_wrap">
+		<img src="/wiki/lib/exe/fetch.php?w=20&amp;tok=af597e&amp;media=menu:interrogation.png" class="media" alt="" width="20" />
+		<h3><a href="<?php echo path_to_wiki; ?>doku.php?id=presentation:sommaire" class="wikilink1" title="Découverte">Découverte</a></h3>
+		Découvrir la randonnée légère et comment débuter.
+	</div><!-- .wrap_box plugin_wrap -->
+
+	<div class="wrap_box plugin_wrap">
+		<img src="/wiki/lib/exe/fetch.php?w=20&amp;tok=af597e&amp;media=menu:interrogation.png" class="media" alt="" width="20" />
+		<h3><a href="<?php echo path_to_wiki; ?>doku.php?id=faq_de_la_randonnee_legere_mul" class="wikilink1" title="FAQ">FAQ</a></h3>
+		Tout ce que vous avez toujours voulu savoir sur la randonnée légère.
+	</div><!-- .wrap_box plugin_wrap -->
+
+	<div class="wrap_box plugin_wrap">
+		<img src="/wiki/lib/exe/fetch.php?media=menu:minilogolbleu.png" class="media" alt="">
+		<h3><a href="<?php echo path_to_wiki; ?>doku.php?id=association_rl:association_randonner_leger" class="wikilink1" title="L'association">L'association</a></h3>
+		Devenir adhérent et participer aux activités qu'elle organise.
+	</div><!-- .wrap_box plugin_wrap -->
+
+	<div class="wrap_box plugin_wrap">
+		<img src="/wiki/lib/exe/fetch.php?w=20&amp;tok=73a246&amp;media=menu:wiki.png" class="media" alt="" width="20" />
+		<h3><a href="<?php echo path_to_wiki; ?>doku.php?id=accueil_du_wiki" class="wikilink1" title="Wiki">Wiki</a></h3>
+		Consulter, enrichir ces connaissances.
+	</div><!-- .wrap_box plugin_wrap -->
+
+	<div class="wrap_box plugin_wrap">
+		<img src="/wiki/lib/exe/fetch.php?w=20&amp;tok=fcabcb&amp;media=menu:forum.png" class="media" alt="" width="20" />
+		<h3><a href="<?php echo path_to_forum; ?>" class="urlextern" title="Forum">Forum</a></h3>
+		Discuter avec des passionnés.
+	</div><!-- .wrap_box plugin_wrap -->
+
+	<div class="wrap_box plugin_wrap">
+		<img src="/wiki/lib/exe/fetch.php?w=20&amp;tok=b98309&amp;media=menu:notice.png" class="media" alt="" width="20" />
+		<h3><a href="<?php echo path_to_wiki; ?>doku.php?id=guide:sommaire" class="wikilink1" title="Guide d'utilisation">Guide d'utilisation</a></h3>
+		Comment fonctionne ce site.
+	</div><!-- .wrap_box plugin_wrap -->
+
+</div>
+
+<figure>
+	<img src="/wiki/lib/exe/fetch.php?media=divers:accueil3.jpg"
+		title="Islande 2008, traversée est-ouest en autonomie "
+		alt="Islande 2008, traversée est-ouest en autonomie " />
+		<figcaption>Photo: Islande 2008, <a href="/perso/doku.php?id=liste_equipement_islande" class="urlextern" title="Liste equipement islande">l'équipement</a>,
+			<a href="/perso/doku.php?id=traversee_de_l_islande_est-ouest_en_autonomie" class="urlextern" title="Traversée de l'islande est-ouest en autonomie">l'itinéraire et autres infos</a>.
+		</figcaption>
+</figure>
---
+++ index_feeds.php
@@ -0,0 +1,40 @@
+<?php
+ini_set('user_agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1');
+
+function GetFeed($url) {
+	$xml = file_get_contents($url);
+
+	$posts = new SimpleXMLElement($xml);
+
+	echo '		<ul>';
+	foreach($posts->topic as $topic) {
+
+	    echo '<li><a href="'.$topic->link.'"><b>'.$topic->title.'</b></a>';
+
+	    // $post = strip_tags($topic->content);
+	    // if(strlen($post) > 50) {
+	    //     echo '<br />' . substr($post, 0, 150).'...<br />';
+	    // }
+	    // else {
+	    //     echo '<br />' . $post.'<br />';
+	    // }
+
+	    // echo ' - <a href="'.$topic->author->uri.'">'.$topic->author->name.'</a></li>';
+	}
+	echo '		</ul>';
+
+}
+
+echo '<h2>Sur les forums</h2>';
+echo '<div class="flex-container grid-2">';
+
+	echo '	<div class="wrap_box plugin_wrap"><h3>Les derniers Messages</h3>';
+		GetFeed('https://www.randonner-leger.org/forum/extern.php?action=feed&type=xml&show=10');
+	echo '	</div>';
+
+	echo '	<div class="wrap_box plugin_wrap"><h3>Les dernières sorties</h3>';
+		GetFeed('https://www.randonner-leger.org/forum/extern.php?action=feed&fid=57&type=xml&show=10');
+	echo '	</div>';
+
+echo '</div>';
+?>
